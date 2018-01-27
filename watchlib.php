1e0d31963f25b66c908c3222691ea34a81005534
akuzo2525 <a.ozuka@gmail.com>
2018-01-27 20:05:08 +0900

    ログインしていて動画が無いときにもtrueを返す

--- watchlib.php
+++ watchlib.php
@@ -3,21 +3,23 @@
 function get_nicohistory($video_id, $user_session)
 {
 	$url = "http://nicovideo.jp/watch/$video_id";
-//	$url = "http://nicovideo.jp/watch/sm32473973";
 	$options = array('http'=>array('method'=>"HEAD", 'header'=>"Accept-language: ja\r\nCookie: user_session=$user_session\r\n"));
 	$context = stream_context_create($options);
 	$data = file_get_contents($url, false, $context);
 
-	foreach($http_response_header as $res)
+	$logined = false;
+	foreach($http_response_header as $header)
 	{
-	//	echo "@ $res\n";
-		preg_match("(Set-Cookie: nicohistory=(.+?);)", $res, $tmp);
-		if(count($tmp) >= 2)
+		if(preg_match("(Set-Cookie: nicohistory=.+?;)", $header, $matches) === 1)
 		{
 			return true;
 		}
+		if(preg_match("/x-niconico-id:\s*\d+/", $header, $matches) === 1)
+		{
+			$logined = true;
+		}
 	}
-	return false;
+	return $logined;
 }
 
 ?>
